99b8337b3dc36a13401191945aeac1e20da9f9d8
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-03 17:46:27 +0530

    add: employee selecting

--- src/views/employees_manager/employees/Table.tsx
+++ src/views/employees_manager/employees/Table.tsx
@@ -1,11 +1,6 @@
 import { useQuery }                                                         from '@tanstack/solid-query';
 import {
-    createColumnHelper,
-    createSolidTable,
-    flexRender,
-    getCoreRowModel,
-    getFilteredRowModel,
-    getSortedRowModel
+    createColumnHelper, createSolidTable, flexRender, getCoreRowModel, getFilteredRowModel, getSortedRowModel
 }                                                                           from '@tanstack/solid-table';
 import { BsSearch, BsSortDown, BsSortUp }                                   from 'solid-icons/bs';
 import { TiArrowUnsorted }                                                  from 'solid-icons/ti';
@@ -13,14 +8,17 @@ import { createEffect, createMemo, createSignal, For, Match, Show, Switch } from
 import EmployeeStatusChip, {
     EEmployeeStatus
 }                                                                           from '../../../common_components/EmployeeStatusChip';
+import { useCNTXAuth }                                                      from '../../../contexts/cntx_auth';
 import api                                                                  from '../../../wretch/api';
 import { Employee }                                                         from '../../../wretch/api/types';
+import { useContextEmployeesManager }                                       from '../context'
 
 
 
 export default function Table() {
-    const columnBuildHelper = createColumnHelper<Employee>()
-    const queryEmployees    = useQuery(() => {
+    const columnBuildHelper         = createColumnHelper<Employee>()
+    const { setSelectedEmployeeId } = useContextEmployeesManager()
+    const queryEmployees            = useQuery(() => {
         return {
             queryKey: [ 'employees' ],
             queryFn : async () => {
@@ -37,18 +35,13 @@ export default function Table() {
             enableColumnFilter: true
         }),
         columnBuildHelper.accessor((row) => `${ row.employee_first_name ??
-                                                'not-registered' } ${ row.employee_last_name ??
-                                                                      'not-registered' }`, {
+                                                'not-registered' } ${ row.employee_last_name ?? 'not-registered' }`, {
                                        id                : 'employee_name',
                                        header            : () => <pre>Name</pre>,
                                        sortingFn         : 'text',
-                                       cell              : (row) =>
-                                               <pre class={ 'px-4' }>{ row.getValue()
-                                                                          .toString()
-                                                                          .padEnd(
-                                                                                  15,
-                                                                                  ' '
-                                                                          ) }</pre>,
+                                       cell              : (row) => <pre class={ 'px-4' }>{ row.getValue()
+                                                                                               .toString()
+                                                                                               .padEnd(15, ' ') }</pre>,
                                        enableSorting     : true,
                                        enableResizing    : true,
                                        enableColumnFilter: true
@@ -67,8 +60,7 @@ export default function Table() {
             header            : () => <pre>Area</pre>,
             enableSorting     : true,
             sortingFn         : 'text',
-            cell              : (row) => <pre>{ row.getValue()?.toString() ??
-                                                'not-set' }</pre>,
+            cell              : (row) => <pre>{ row.getValue()?.toString() ?? 'not-set' }</pre>,
             enableResizing    : true,
             enableColumnFilter: true
         }),
@@ -137,71 +129,66 @@ export default function Table() {
     return <table class={ 'w-full h-full flex flex-col items-stretch justify-start overflow-visible' }>
         <thead>
         <For each={ getHeaderGroups() }>
-            {
-                (headerGroup) => {
-                    return <tr class={ 'w-full h-fit flex flex-row items-stretch justify-start bg-teal-200/30 border-2 border-teal-400/60 rounded-t-md [&>*:not(:last-child)]:border-r-1 [&>*]:border-teal-400/60' }>
-                        <For each={ headerGroup.headers }>
-                            {
-                                (header) => {
-                                    return <th
-                                            class={ `flex-1 py-2 px-2 ${ header.column.getCanSort()
-                                                                         ? 'cursor-pointer'
-                                                                         : undefined } flex flex-col items-center justify-center gap-2` }
-                                            style={ { 'min-width': `${ header.getSize() }px` } }
-                                    >
-                                        <div
-                                                class={ 'flex flex-row items-center justify-start gap-4' }
-                                                onClick={ header.column.getToggleSortingHandler() }
-                                        >
-                                            <Switch fallback={ <TiArrowUnsorted/> }>
-                                                <Match when={ header.column.getIsSorted() === 'asc' }>
-                                                    <BsSortDown/>
-                                                </Match>
-                                                <Match when={ header.column.getIsSorted() === 'desc' }>
-                                                    <BsSortUp/>
-                                                </Match>
-                                            </Switch>
-                                            { flexRender(header.column.columnDef.header, header.getContext()) }
-                                        </div>
-                                        <div>
-                                            <Show when={ header.column.getCanFilter() }>
-                                                <ColumnFilter getFilterValue={ header.column.setFilterValue }/>
-                                            </Show>
-                                        </div>
-                                    </th>
-                                }
-                            }
-                        </For>
-                    </tr>
-                }
-            }
+            { (headerGroup) => {
+                return <tr class={ 'w-full h-fit flex flex-row items-stretch justify-start bg-teal-200/30 border-2 border-teal-400/60 rounded-t-md [&>*:not(:last-child)]:border-r-1 [&>*]:border-teal-400/60' }>
+                    <For each={ headerGroup.headers }>
+                        { (header) => {
+                            return <th
+                                    class={ `flex-1 py-2 px-2 ${ header.column.getCanSort()
+                                                                 ? 'cursor-pointer'
+                                                                 : undefined } flex flex-col items-center justify-center gap-2` }
+                                    style={ { 'min-width': `${ header.getSize() }px` } }
+                            >
+                                <div
+                                        class={ 'flex flex-row items-center justify-start gap-4' }
+                                        onClick={ header.column.getToggleSortingHandler() }
+                                >
+                                    <Switch fallback={ <TiArrowUnsorted/> }>
+                                        <Match when={ header.column.getIsSorted() === 'asc' }>
+                                            <BsSortDown/>
+                                        </Match>
+                                        <Match when={ header.column.getIsSorted() === 'desc' }>
+                                            <BsSortUp/>
+                                        </Match>
+                                    </Switch>
+                                    { flexRender(header.column.columnDef.header, header.getContext()) }
+                                </div>
+                                <div>
+                                    <Show when={ header.column.getCanFilter() }>
+                                        <ColumnFilter getFilterValue={ header.column.setFilterValue }/>
+                                    </Show>
+                                </div>
+                            </th>
+                        } }
+                    </For>
+                </tr>
+            } }
         </For>
         </thead>
         
         <tbody class={ 'flex flex-col items-stretch justify-start bg-teal-200/30 border-x-2 border-b-2 border-teal-400/60 [&>*:not(:last-child)]:border-b-1 [&>*:not(:last-child)]:border-teal-400/60 transform-3d perspective-near perspective-origin-center overflow-visible' }>
         
         <For each={ getRowModel().rows }>
-            {
-                (row) => {
-                    return <tr class={ 'px-2 flex flex-row items-stretch justify-start hover:bg-yellow-200/60 transition-all duration-300' }>
-                        
-                        <For each={ row.getVisibleCells() }>
-                            {
-                                (cell) => {
-                                    return <td
-                                            class={ 'flex-1 py-2 flex flex-col items-center justify-center text-center' }
-                                    >
-                                        {
-                                            flexRender(cell.column.columnDef.cell, cell.getContext())
-                                        }
-                                    </td>
-                                }
-                            }
-                        </For>
+            { (row) => {
+                return <tr
+                        class={ 'px-2 flex flex-row items-stretch justify-start hover:bg-yellow-200/60 transition-all duration-300 cursor-pointer' }
+                        onClick={ () => {
+                            setSelectedEmployeeId(row.original.employee_id)
+                        } }
+                >
                     
-                    </tr>
-                }
-            }
+                    <For each={ row.getVisibleCells() }>
+                        { (cell) => {
+                            return <td
+                                    class={ 'flex-1 py-2 flex flex-col items-center justify-center text-center' }
+                            >
+                                { flexRender(cell.column.columnDef.cell, cell.getContext()) }
+                            </td>
+                        } }
+                    </For>
+                
+                </tr>
+            } }
         </For>
         
         </tbody>
