5081b82f029c5535164ff992db5a7932ad9fe376
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-12 12:43:11 +0530

    add conditional item rendering

--- src/views/employees_manager/sales_groups/index.tsx
+++ src/views/employees_manager/sales_groups/index.tsx
@@ -2,7 +2,8 @@ import { Accordion }           from '@kobalte/core/accordion';
 import { useQuery }            from '@tanstack/solid-query';
 import {
     createSignal,
-    For
+    For,
+    Show
 }                              from 'solid-js';
 import { toast }               from 'solid-toast';
 import api                     from '../../../wretch/api';
@@ -19,55 +20,56 @@ export default function SalesGroups() {
     const [ getIsAddSalesGroupDialogOpen, setIsAddSalesGroupDialogOpen ]       = createSignal(false)
     const [ getIsRemoveSalesGroupDialogOpen, setIsRemoveSalesGroupDialogOpen ] = createSignal(false)
     
-    const salesGroups = useQuery(() => {
+    const query_SalesGroups = useQuery(() => {
         return {
             queryKey: [ 'sales_groups' ],
-            async queryFn() {
+            queryFn : async () => {
                 return await api.SalesGroupApi.getAll()
             },
         }
     })
     
     return <div>
-        <Accordion
-                defaultValue={ [] }
-                multiple={ true }
-                class={ 'w-full max-w-full px-4 h-fit flex flex-col sales_group.-stretch justify-start gap-1 overflow-x-visible' }
+        <Show
+                when={ query_SalesGroups.data && query_SalesGroups.data.length > 0 }
+                fallback={ <div>
+                    <pre>No Sales Groups Created</pre>
+                </div> }
         >
-            <For each={ salesGroups.data }>
-                { (sales_group) => {
-                    if (!sales_group || !sales_group.sales_group_id) {
-                        return
-                    }
-                    
-                    return (<Accordion.Item
-                            value={ sales_group.sales_group_id }
-                            class={ 'w-full h-fit min-h-12 rounded-md transform-3d perspective-normal perspective-origin-top overflow-x-visible' }
-                    >
+            <Accordion
+                    defaultValue={ [] }
+                    multiple={ true }
+                    class={ 'w-full max-w-full px-4 h-fit flex flex-col sales_group.-stretch justify-start gap-1 overflow-x-visible' }
+            >
+                
+                <For each={ query_SalesGroups.data }>
+                    { (sales_group) => {
+                        if (!sales_group || !sales_group.sales_group_id) {
+                            return
+                        }
                         
-                        <Accordion.Header class={ 'w-full h-fit rounded-md bg-teal-300/60 hover:bg-yellow-300/60 hover:translate-z-3 hover:shadow-[0px_2px_4px_2px] shadow-teal-600/10 transition-all ease-in-out duration-300' }>
-                            <Accordion.Trigger class={ 'w-full h-fit' }>
-                                <SalesGroupHeader
-                                        sales_group_name={ sales_group.sales_group_name }
-                                        sales_group_members={ sales_group.sales_group_members ?? {
-                                            active: 0,
-                                            total : 0
-                                        } }
-                                        sales_group_sales={ sales_group.sales_group_sales ?? {
-                                            this_month: 0,
-                                            last_month: 0
-                                        } }
-                                />
-                            </Accordion.Trigger>
-                        </Accordion.Header>
-                        
-                        <Accordion.Content class={ 'w-full h-fit max-h-[300px] px-2 flex flex-col sales_group.-stretch justify-start gap-1 bg-teal-600/60 overflow-y-scroll' }>
-                            <SalesGroupDetails salesGroupId={ sales_group.sales_group_id }/>
-                        </Accordion.Content>
-                    </Accordion.Item>)
-                } }
-            </For>
-        </Accordion>
+                        return (<Accordion.Item
+                                value={ sales_group.sales_group_id }
+                                class={ 'w-full h-fit min-h-12 rounded-md transform-3d perspective-normal perspective-origin-top overflow-x-visible' }
+                        >
+                            
+                            <Accordion.Header class={ 'w-full h-fit rounded-md bg-teal-300/60 hover:bg-yellow-300/60 hover:translate-z-3 hover:shadow-[0px_2px_4px_2px] shadow-teal-600/10 transition-all ease-in-out duration-300' }>
+                                <Accordion.Trigger class={ 'w-full h-fit' }>
+                                    <SalesGroupHeader
+                                            sales_group_id={ sales_group.sales_group_id }
+                                            sales_group_name={ sales_group.sales_group_name }
+                                    />
+                                </Accordion.Trigger>
+                            </Accordion.Header>
+                            
+                            <Accordion.Content class={ 'w-full h-fit max-h-[300px] px-2 flex flex-col sales_group.-stretch justify-start gap-1 bg-teal-600/60 overflow-y-scroll' }>
+                                <SalesGroupDetails salesGroupId={ () => sales_group.sales_group_id }/>
+                            </Accordion.Content>
+                        </Accordion.Item>)
+                    } }
+                </For>
+            </Accordion>
+        </Show>
         
         <Dialog_RemoveSalesGroup
                 setIsBusy={ setIsBusy }
