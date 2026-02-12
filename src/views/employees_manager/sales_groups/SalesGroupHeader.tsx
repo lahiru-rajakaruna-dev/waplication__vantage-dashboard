c4d7c8ba0e3101408c1d8c659761c93d7454db00
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-12 12:43:40 +0530

    delegated sales_group information fetching

--- src/views/employees_manager/sales_groups/SalesGroupHeader.tsx
+++ src/views/employees_manager/sales_groups/SalesGroupHeader.tsx
@@ -1,48 +1,103 @@
+import { useQuery }      from '@tanstack/solid-query';
 import { FaSolidUsers }  from 'solid-icons/fa';
 import { OcChevrondown } from 'solid-icons/oc';
+import {
+    createMemo,
+    Show
+}                        from 'solid-js';
+import api               from '../../../wretch/api';
 
 
 
 export default function SalesGroupHeader(props: {
+    sales_group_id: string,
     sales_group_name: string,
-    sales_group_members: {
-        active: number,
-        total: number
-    },
-    sales_group_sales: {
-        this_month: number,
-        last_month: number
-    }
 }) {
+    const query_SalesGroupEmployees = useQuery(() => {
+        return {
+            queryKey: [
+                'sales_group',
+                'employees',
+                props.sales_group_id
+            ],
+            queryFn : async () => {
+                return api.EmployeeApi.getBySalesGroup(props.sales_group_id)
+            }
+        }
+    })
+    
+    const activeMembers = createMemo(() => {
+        if (!query_SalesGroupEmployees.data || query_SalesGroupEmployees.data.length <= 0) {
+            return []
+        }
+        
+        return query_SalesGroupEmployees.data.filter((employee) => {
+            return employee.employee_status === 'ON_FIELD'
+        })
+    })
+    
     return (<div class={ 'w-full h-fit px-3 py-3 flex flex-row item-center justify-between gap-12' }>
         <div class={ 'w-fit max-w-2/12 font-semibold text-left text-v-text-body' }>
-            <pre>{ props.sales_group_name.padEnd(20, ' ') }</pre>
+            <pre>{ props.sales_group_name.padEnd(
+                    20,
+                    ' '
+            ) }</pre>
         </div>
         <div class={ 'w-full' }/>
         <div class={ 'w-fit max-w-3/12 flex flex-row item-center justify-end gap-1' }>
             <div class={ 'w-full h-full flex flex-row items-center justify-start gap-2' }>
                 <FaSolidUsers size={ 18 }/>
-                <div class={ 'flex flex-row items-center justify-start gap-1' }>
+                <Show
+                        when={ query_SalesGroupEmployees.data && query_SalesGroupEmployees.data.length > 0 }
+                        fallback={ <div>
+                        <pre>
+                            No Data
+                        </pre>
+                        </div> }
+                >
+                    <div class={ 'flex flex-row items-center justify-start gap-1' }>
                 <pre>
-                        { props.sales_group_members.active.toString()
-                               .padStart(3, '0') }
+                        { activeMembers()
+                                .length
+                                .toString()
+                                .padStart(
+                                        3,
+                                        '0'
+                                ) }
                     </pre>
-                    <div>/</div>
-                    <pre>
-                        { props.sales_group_members.total.toString()
-                               .padStart(3, '0') }
+                        <div>/</div>
+                        <pre>
+                        { query_SalesGroupEmployees.data.length.toString()
+                                                   .padStart(
+                                                           3,
+                                                           '0'
+                                                   ) }
                     </pre>
-                </div>
+                    </div>
+                </Show>
             </div>
         </div>
         <div class={ 'w-fit max-w-2/12 text-left whitespace-nowrap flex flex-col items-stretch justify-center' }>
                     <pre class={ 'font-semibold' }>
-                    { `Rs: ${ props.sales_group_sales.last_month.toString()
-                                   .padStart(6, '0') }` }
+{/*
+ { `Rs: ${ props.sales_group_sales.last_month.toString()
+ .padStart(
+ 6,
+ '0'
+ ) }` }
+ 
+ */ }
+                        50,000
                     </pre>
             <pre class={ 'text-sm font-semibold text-v-text-body' }>
-                    { `Rs: ${ props.sales_group_sales.this_month.toString()
-                                   .padStart(6, '0') }` }
+{/*
+ { `Rs: ${ props.sales_group_sales.this_month.toString()
+ .padStart(
+ 6,
+ '0'
+ ) }` }
+ */ }
+                87,000
                     </pre>
         </div>
         
@@ -55,7 +110,11 @@ export default function SalesGroupHeader(props: {
                                              ) }/=` }
              </pre>
             <pre class={ 'text-sm font-semibold text-v-text-body' }>
-                { `Last Month(x1K): ${ (500).toString().padStart(3, '0') }/=` }
+                { `Last Month(x1K): ${ (500).toString()
+                                            .padStart(
+                                                    3,
+                                                    '0'
+                                            ) }/=` }
             </pre>
         </div>
         
