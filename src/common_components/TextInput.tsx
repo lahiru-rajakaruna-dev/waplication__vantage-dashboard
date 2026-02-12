90cbf4be536886c9d9870944a097c70ee2337fcd
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-12 12:24:16 +0530

    add datalist

--- src/common_components/TextInput.tsx
+++ src/common_components/TextInput.tsx
@@ -1,5 +1,6 @@
-import { TextField } from '@kobalte/core/text-field';
-import { JSX }       from 'solid-js';
+import { TextField }  from '@kobalte/core/text-field';
+import { JSX }        from 'solid-js';
+import { v4 as uuid } from 'uuid'
 
 
 
@@ -11,25 +12,32 @@ export default function TextInput(props: {
     dataDataValidationFunction?: (value: string) => 'valid' | 'invalid'
     errorMessage?: string
     inputConfig: Pick<JSX.InputHTMLAttributes<HTMLInputElement>, 'type'>
+    datalist?: string[]
 }) {
+    const id = uuid()
     
-    return (
-            <TextField
-                    class={ 'flex flex-col items-stretch justify-start gap-1' }
-                    onChange={ (value) => {
-                        props.onChange(value)
-                    } }
-            >
-                {
-                    props.label ? <pre class={ 'text-sm text-v-text-body' }>{ props.label }</pre> : <></>
-                }
-                <TextField.Input
-                        type={ props.inputConfig.type }
-                        class={ 'w-full h-fit px-4 py-2 border-2 border-teal-400/60 rounded-md bg-teal-200/30 text-sm font-semibold outline-0 backdrop-blur-xs shadow-inner' }
-                        placeholder={ props.placeholder }
-                        value={ props.value }
-                />
-                <TextField.ErrorMessage>{ props.errorMessage ?? 'Invalid data...' }</TextField.ErrorMessage>
-            </TextField>
-    )
+    return (<TextField
+            class={ 'flex flex-col items-stretch justify-start gap-1' }
+            onChange={ (value) => {
+                props.onChange(value)
+            } }
+    >
+        { props.label
+          ? <pre class={ 'text-sm text-v-text-body' }>{ props.label }</pre>
+          : <></> }
+        <TextField.Input
+                type={ props.inputConfig.type }
+                class={ 'w-full h-fit px-4 py-2 border-2 border-teal-400/60 rounded-md bg-teal-200/30 text-sm font-semibold outline-0 backdrop-blur-xs shadow-inner' }
+                placeholder={ props.placeholder }
+                value={ props.value }
+                list={ `input-data-list-${ id }` }
+        
+        />
+        <TextField.ErrorMessage>{ props.errorMessage ?? 'Invalid data...' }</TextField.ErrorMessage>
+        <datalist id={ `input-data-list-${ id }` }>
+            { props.datalist?.map((value) => {
+                return <option value={ value }/>
+            }) }
+        </datalist>
+    </TextField>)
 }
