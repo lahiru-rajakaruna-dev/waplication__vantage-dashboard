ddecb6bc5c6070de6ce67f6f2e410953a2349de7
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-12 12:42:42 +0530

    formatting changes

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,16 +1,30 @@
-import { Tabs }                                                                                  from '@kobalte/core/tabs';
-import { BiSolidDashboard }                                                                      from 'solid-icons/bi';
-import { FaSolidBoxesPacking, FaSolidChartColumn, FaSolidGears, FaSolidHandshake, FaSolidTruck } from 'solid-icons/fa';
-import { TiGroup }                                                                               from 'solid-icons/ti';
-import { createSignal, ErrorBoundary, For, JSX }                                                 from 'solid-js';
-import ApplicationHeader
-                                                                                                 from '../common_components/ApplicationHeader';
-import EmployeesManager
-                                                                                                 from './employees_manager';
+import { Tabs }             from '@kobalte/core/tabs';
+import { BiSolidDashboard } from 'solid-icons/bi';
+import {
+    FaSolidBoxesPacking,
+    FaSolidChartColumn,
+    FaSolidGears,
+    FaSolidHandshake,
+    FaSolidTruck
+}                           from 'solid-icons/fa';
+import { TiGroup }          from 'solid-icons/ti';
+import {
+    createSignal,
+    ErrorBoundary,
+    For,
+    JSX
+}                           from 'solid-js';
+import ApplicationHeader    from '../common_components/ApplicationHeader';
+import EmployeesManager     from './employees_manager';
 
 
 
-const tabs: Array<{ key: string, title: string, component: () => JSX.Element, icon: () => JSX.Element }> = [
+const tabs: Array<{
+    key: string,
+    title: string,
+    component: () => JSX.Element,
+    icon: () => JSX.Element
+}> = [
     {
         key      : 'dashboard',
         title    : 'Dashboard',
@@ -47,26 +61,26 @@ const tabs: Array<{ key: string, title: string, component: () => JSX.Element, ic
         
     },
     {
-        key       : 'clients',
-        title     : 'Clients',
-        components: () => <div>Clients</div>,
-        icon      : () => <FaSolidHandshake
+        key      : 'clients',
+        title    : 'Clients',
+        component: () => <div>Clients</div>,
+        icon     : () => <FaSolidHandshake
                 size={ 32 }
         />
     },
     {
-        key       : 'sales',
-        title     : 'Sales',
-        components: () => <div>Sales</div>,
-        icon      : () => <FaSolidChartColumn
+        key      : 'sales',
+        title    : 'Sales',
+        component: () => <div>Sales</div>,
+        icon     : () => <FaSolidChartColumn
                 size={ 32 }
         />
     },
     {
-        key       : 'settings',
-        title     : 'Settings',
-        components: () => <div>Settings</div>,
-        icon      : () => <FaSolidGears
+        key      : 'settings',
+        title    : 'Settings',
+        component: () => <div>Settings</div>,
+        icon     : () => <FaSolidGears
                 size={ 32 }
         />
     },
@@ -78,19 +92,19 @@ export default function Views() {
     return (<div class={ 'z-0 w-full h-full grid grid-cols-12 grid-rows-12 bg-white' }>
         {/* HEADER BAR*/ }
         <ErrorBoundary
-                fallback={
-                    <div>
-                        <pre>Something Went Wrong</pre>
-                        <pre>Check the console</pre>
-                    </div>
-                }>
+                fallback={ <div>
+                    <pre>Something Went Wrong</pre>
+                    <pre>Check the console</pre>
+                </div> }
+        >
             <ApplicationHeader/>
         </ErrorBoundary>
         <ErrorBoundary
                 fallback={ <div>
                     <pre>Something went wrong</pre>
                     <pre>Check the console</pre>
-                </div> }>
+                </div> }
+        >
             <div
                     class={ 'col-start-1 col-end-13 row-start-2 row-end-13 relative bg-white/10 backdrop-blur-sm' }
             >
@@ -101,33 +115,45 @@ export default function Views() {
                 >
                     <Tabs.List class={ 'z-10 w-3/12 min-w-56 max-w-60  h-full flex flex-col items-stretch justify-start transform-3d perspective-normal perspective-origin-top-left backdrop-blur-sm border-r-2 border-r-v-bg/80 bg-v-accent/30' }>
                         <For
-                                each={ tabs }>
-                            { (item, index) => {
+                                each={ tabs }
+                        >
+                            { (
+                                    item,
+                                    index
+                            ) => {
                                 return <Tabs.Trigger
                                         value={ item.key }
-                                        class={ `py-4 px-3 font-normal last:mt-auto origin-top-left transition-all ${ item.key ===
-                                                                                                                      getSelectedTab() ? 'translate-z-3' : 'translate-none' } ${ item.key ===
-                                                                                                                                                                                 getSelectedTab() ? 'shadow-md' : 'shadow-none' } ${ item.key ===
-                                                                                                                                                                                                                                     getSelectedTab() ? 'bg-yellow-200/60' : 'bg-teal-200/60' } border-b-2 border-v-bg/30 last:border-0 backdrop-blur-sm` }
+                                        class={ `py-4 px-3 font-normal last:mt-auto origin-top-left transition-all ${ item.key === getSelectedTab()
+                                                                                                                      ? 'translate-z-3'
+                                                                                                                      : 'translate-none' } ${ item.key === getSelectedTab()
+                                                                                                                                              ? 'shadow-md'
+                                                                                                                                              : 'shadow-none' } ${ item.key === getSelectedTab()
+                                                                                                                                                                   ? 'bg-yellow-200/60'
+                                                                                                                                                                   : 'bg-teal-200/60' } border-b-2 border-v-bg/30 last:border-0 backdrop-blur-sm` }
                                 >
                                     <div class={ 'w-full h-full flex flex-row items-center justify-start gap-4' }>
                                         <div class={ 'aspect-square h-full w-auto' }>{ item.icon() }</div>
                                         <div
-                                                class={ `w-full h-full flex flex-col items-start justify-center whitespace-nowrap ${ item.key ===
-                                                                                                                                     getSelectedTab() ? 'text-v-text-main font-normal' : 'text-v-text-body' } font-light` }>{ item.title }</div>
+                                                class={ `w-full h-full flex flex-col items-start justify-center whitespace-nowrap ${ item.key === getSelectedTab()
+                                                                                                                                     ? 'text-v-text-main font-normal'
+                                                                                                                                     : 'text-v-text-body' } font-light` }
+                                        >{ item.title }</div>
                                     </div>
                                 </Tabs.Trigger>
                             } }
                         </For>
                     </Tabs.List>
                     <For
-                            each={
-                                tabs
-                            }>
-                        { (item, index) => {
+                            each={ tabs }
+                    >
+                        { (
+                                item,
+                                index
+                        ) => {
                             return <Tabs.Content
                                     value={ item.key }
-                                    class={ 'relative z-0 w-full h-full p-4 bg-white/90 backdrop-blur-sm' }>
+                                    class={ 'relative z-0 w-full h-full p-4 bg-white/90 backdrop-blur-sm' }
+                            >
                                 { item.component() }
                             </Tabs.Content>
                         } }
