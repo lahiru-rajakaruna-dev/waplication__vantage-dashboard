6da89b93ad7a052e9010ac59f534521090f72067
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-14 13:52:06 +0530

    update values to enums

--- src/common_components/EmployeeStatusChip.tsx
+++ src/common_components/EmployeeStatusChip.tsx
@@ -1,36 +1,32 @@
-import { Match, Switch } from 'solid-js';
+import {
+    Match,
+    Switch
+}                              from 'solid-js';
+import { Enum_EmployeeStatus } from '../schemas';
 
-
-
-export enum EEmployeeStatus {
-    ACTIVE   = 'ACTIVE',
-    ON_LEAVE = 'ON_LEAVE',
-    OFF      = 'OFF'
-}
 
 
 export default function EmployeeStatusChip(props: {
-    status: EEmployeeStatus
+    status: Enum_EmployeeStatus
 }) {
     return <Switch
             fallback={
-                <pre class={ 'w-28 px-1 pt-1 py-0.5 text-center text-sm font-semibold text-white/60 bg-emerald-300/60 border-2 border-emerald-600/60 rounded-full cursor-default' }>
+                <pre class={ 'w-32 px-2 pt-1 py-0.5 text-center text-sm font-semibold text-white/60 bg-emerald-300/60 border-2 border-emerald-600/60 rounded-full cursor-default' }>
                     UNKNOWN VALUE
-            </pre>
-            }
+            </pre> }
     >
-        <Match when={ props.status === 'ACTIVE' }>
-            <pre class={ 'w-28 px-1 pt-1 py-0.5 text-center text-sm font-semibold text-white/60 bg-emerald-300/60 border-2 border-emerald-600/60 rounded-full cursor-default' }>
+        <Match when={ props.status === 'ON_FIELD' }>
+            <pre class={ 'w-32 px-2 pt-1 py-0.5 text-center text-sm font-semibold text-white/60 bg-emerald-300/60 border-2 border-emerald-600/60 rounded-full cursor-default' }>
                 { props.status }
             </pre>
         </Match>
         <Match when={ props.status === 'ON_LEAVE' }>
-            <pre class={ 'w-28 px-1 pt-1 py-0.5 text-center text-sm font-semibold text-white/60 rounded-full bg-gray-300/60 border-2 border-gray-600/60 cursor-default' }>
+            <pre class={ 'w-32 px-2 pt-1 py-0.5 text-center text-sm font-semibold text-white/60 rounded-full bg-gray-300/60 border-2 border-gray-600/60 cursor-default' }>
                 { props.status }
             </pre>
         </Match>
-        <Match when={ props.status === 'OFF' }>
-            <pre class={ 'w-28 px-1 pt-1 py-0.5 text-center text-sm font-semibold text-white/60 bg-sky-300/60 border-2 border-sky-600/60 rounded-full cursor-default' }>
+        <Match when={ props.status === 'NOT_REPORTED' }>
+            <pre class={ 'w-32 px-2 pt-1 py-0.5 text-center text-sm font-semibold text-white/60 bg-sky-300/60 border-2 border-sky-600/60 rounded-full cursor-default' }>
                 { props.status }
             </pre>
         </Match>
