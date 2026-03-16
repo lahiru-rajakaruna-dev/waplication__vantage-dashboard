8892fa21204926bdfcbe727bc354e52a028126b1
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-16 12:55:26 +0530

    feat(vite config): made runtime port dynamic

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import tailwindcss    from '@tailwindcss/vite';
-import devtools       from 'solid-devtools/vite';
-import {defineConfig} from 'vite';
-import solidPlugin    from 'vite-plugin-solid';
+import tailwindcss      from '@tailwindcss/vite';
+import devtools         from 'solid-devtools/vite';
+import { defineConfig } from 'vite';
+import solidPlugin      from 'vite-plugin-solid';
 
-export default defineConfig({
-                                plugins: [
-                                    devtools(),
-                                    solidPlugin(),
-                                    tailwindcss()
-                                ],
-                                server : {
-                                    port: 3500,
-                                },
-                                build  : {
-                                    target: 'esnext',
-                                },
-                            });
+
+
+
+
+export default defineConfig(({ mode, }) => {
+    console.log(mode);
+    console.log(process.env);
+    return {
+        plugins: [
+            devtools(),
+            solidPlugin(),
+            tailwindcss()
+        ],
+        server : {
+            port: parseInt(process.env.PORT!) || 3500,
+        },
+        build  : {
+            target: 'esnext',
+        },
+    }
+});
