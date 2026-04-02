4c1d3df29a0cba446a867c48ce54722a605d58e9
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-04-02 21:38:56 +0530

    style(registration module): formatting

--- src/registration/index.tsx
+++ src/registration/index.tsx
@@ -1,17 +1,17 @@
-import { useMutation }      from '@tanstack/solid-query';
-import { FaSolidArrowLeft } from 'solid-icons/fa';
+import {useMutation}      from '@tanstack/solid-query';
+import {FaSolidArrowLeft} from 'solid-icons/fa';
 import {
     Component,
     createSignal
-}                           from 'solid-js';
-import { Dynamic }          from 'solid-js/web';
-import { toast }            from 'solid-toast';
-import PrimaryButton        from '../common_components/PrimaryButton';
-import { useCNTXAuth }      from '../contexts/cntx_auth';
-import api                  from '../wretch/api';
-import Step_1               from './components/Step_1';
-import Step_3               from './components/Step_3';
-import { TStepProps }       from './components/types';
+}                         from 'solid-js';
+import {Dynamic}          from 'solid-js/web';
+import {toast}            from 'solid-toast';
+import PrimaryButton      from '../common_components/PrimaryButton';
+import {useCNTXAuth}      from '../contexts/cntx_auth';
+import api                from '../wretch/api';
+import Step_1             from './components/Step_1';
+import Step_3             from './components/Step_3';
+import {TStepProps}       from './components/types';
 
 
 
@@ -22,8 +22,8 @@ const steps: Component<TStepProps>[] = [
 
 export default function OrganizationRegistration() {
     const {userProfile}          = useCNTXAuth()
-    const [ getStep, setStep ]       = createSignal(0)
-    const [ getData, setData ]       = createSignal<{
+    const [getStep, setStep]     = createSignal(0)
+    const [getData, setData]     = createSignal<{
         organization_name: string
         organization_admin_email: string,
         organization_admin_phone: string,
@@ -34,8 +34,8 @@ export default function OrganizationRegistration() {
            organization_admin_email: userProfile()?.email ?? '',
            organization_admin_phone: '',
        })
-    const [ getIsBusy, setIsBusy ]   = createSignal<boolean>(false)
-    
+    const [getIsBusy, setIsBusy] = createSignal<boolean>(false)
+
     const mutationAddOrganization = useMutation(() => {
         return {
             mutationKey: [
@@ -73,8 +73,7 @@ export default function OrganizationRegistration() {
             },
         }
     })
-    
-    
+
     async function submitOrganizationData() {
         setIsBusy(true)
         try {
@@ -84,12 +83,12 @@ export default function OrganizationRegistration() {
                       organization_logo_url,
                       organization_name
                   } = getData()
-            
+
             if (!organization_name || !organization_admin_email || !organization_admin_phone || !organization_logo_url) {
                 toast.error('[-] Incomplete registration data...')
                 return
             }
-            
+
             mutationAddOrganization.mutate()
         } catch (e) {
             throw new Error('Registration failed...')
@@ -97,8 +96,7 @@ export default function OrganizationRegistration() {
             setIsBusy(false)
         }
     }
-    
-    
+
     function storeData(data: Record<string, any>) {
         setData((prevData) => {
             return {
@@ -106,53 +104,52 @@ export default function OrganizationRegistration() {
             }
         })
     }
-    
-    
-    return <div class={ 'relative w-full h-full flex flex-col items-center justify-center gap-12' }>
-        <div class={ 'absolute top-1/12 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl font-black' }>VANTAGE</div>
-        <div class={ 'w-fit min-w-lg h-fit p-8 flex flex-col items-stretch justify-start gap-4 bg-v-bg/80 shadow-[inset_0px_0px_1px_3px] shadow-v-accent-glow backdrop-blur-md rounded-md ' }>
+
+    return <div class={'relative w-full h-full flex flex-col items-center justify-center gap-12'}>
+        <div class={'absolute top-1/12 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl font-black'}>VANTAGE</div>
+        <div class={'w-fit min-w-lg h-fit p-8 flex flex-col items-stretch justify-start gap-4 bg-v-bg/80 shadow-[inset_0px_0px_1px_3px] shadow-v-accent-glow backdrop-blur-md rounded-md '}>
             <Dynamic
-                    component={ steps[getStep()] }
-                    onButtonClick={ async (data) => {
+                    component={steps[getStep()]}
+                    onButtonClick={async (data) => {
                         storeData(data);
-                        
+
                         if (getStep() >= steps.length - 1) {
                             await submitOrganizationData()
                             return
                         }
-                        
+
                         if (getIsBusy()) {
                             toast('Please wait for a moment...')
                             return
                         }
-                        
+
                         setStep((prevStep) => {
                             if (prevStep >= steps.length - 1) {
                                 return prevStep
                             }
-                            
+
                             return prevStep + 1
                         })
-                    } }
-                    getIsBusy={ getIsBusy }
-                    setIsBusy={ setIsBusy }
+                    }}
+                    getIsBusy={getIsBusy}
+                    setIsBusy={setIsBusy}
             />
-            <div class={ 'w-full h-fit flex flex-col items-stretch justify-center' }>
+            <div class={'w-full h-fit flex flex-col items-stretch justify-center'}>
                 <PrimaryButton
-                        onClick={ () => {
+                        onClick={() => {
                             setStep((prevStep) => {
                                 if (prevStep <= 0) {
                                     return prevStep;
                                 }
-                                
+
                                 return prevStep - 1
                             })
-                        } }
-                        getIsBusy={ getIsBusy }
-                        getIsDisabled={ getIsBusy }
+                        }}
+                        getIsBusy={getIsBusy}
+                        getIsDisabled={getIsBusy}
                 >
                     <PrimaryButton.Icon>
-                        <FaSolidArrowLeft size={ 14 }/>
+                        <FaSolidArrowLeft size={14}/>
                     </PrimaryButton.Icon>
                     <PrimaryButton.Label>
                         <pre>Go Back</pre>
