b147287ee4849f837d4692da6900d4909e382208
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-04-03 22:02:57 +0530

    feat(wretch auth api): add authenticateApi fn

--- src/wretch/api/authorization.ts
+++ src/wretch/api/authorization.ts
@@ -5,7 +5,18 @@ import { wretchInstance } from '../index';
 
 
 export const AuthorizationApi = {
-    isRegistered: async () => {
-        return await wretchInstance.get('/auth/is_registered').json<{ isRegistered: boolean }>()
-    }
+    isRegistered   : async () => {
+        return await wretchInstance
+            .get(
+                '/auth/is_registered'
+            )
+            .json<{ isRegistered: boolean }>()
+    },
+    authenticateApi: async () => {
+        return await wretchInstance
+            .get('/auth/authenticate').json<boolean>()
+    },
+    // refreshAccessToken: async () => {
+    //     return await wretchInstance.get('/auth/refresh_access_token').json<{access_token:}>()
+    // }
 }
