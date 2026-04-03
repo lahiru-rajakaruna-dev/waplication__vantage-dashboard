03957bd577a12c9e787a2810e3d067ab869efff4
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-04-03 22:10:02 +0530

    refactor(app module): removed auth check

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import {Show,}                  from 'solid-js';
-import {Toaster}                from 'solid-toast';
-import {useCNTXAuth}            from './contexts/cntx_auth';
-import OrganizationRegistration from './registration';
-import Views                    from './views';
+import {Toaster} from 'solid-toast';
+import Views     from './views';
 
 
 
 export default function App() {
-    const {
-              isAuthenticated,
-              isRegistered
-          } = useCNTXAuth()
 
     return (<div class={'w-full h-screen min-h-screen mx-auto bg-gradient-to-b from-v-bg to-v-accent'}>
         <Toaster/>
-        <Show
-                when={isRegistered()}
-                // when={ true } // FOR DEVELOPMENT
-                fallback={<OrganizationRegistration/>}
-        >
-            <Views/>
-        </Show>
+        <Views/>
     </div>)
 }
